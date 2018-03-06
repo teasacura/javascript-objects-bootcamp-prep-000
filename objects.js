39273edffc3c1f9a5c555e7fb7abcedae013cd3b
Eva Leake <teasacuraster@gmail.com>
2018-03-06 16:13:30 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 function removeFromPlaylist(playlist, artistName) {
   var name = artistName
-  delete playlist['name'];
+  delete playlist.name;
   return playlist;
 }
