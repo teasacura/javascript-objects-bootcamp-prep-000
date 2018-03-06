1a4e4c19144aea3c365718722fac4a8426d63abf
Eva Leake <teasacuraster@gmail.com>
2018-03-06 15:44:12 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = { 'Green Day': 'American Idiot'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, { artistName: songTitle })
+  Object.assign({}, playlist, { artistName: songTitle })
+  return playlist
 }
